2c65616e3e4faf0ef677568bd5b17f9927264146
BTavkhelidze <125907215+BTavkhelidze@users.noreply.github.com>
2025-07-12 18:35:13 +0400

    feat/Header Section

---
+++ public/index.ts
@@ -0,0 +1,6 @@
+import LogoSVG from './logo.svg';
+import ShopSVG from './shop.svg';
+import ProfileSVG from './profile.svg';
+import SearchSVG from './searchIcon.svg';
+
+export { LogoSVG, ShopSVG, ProfileSVG, SearchSVG };
--- src/sections/HeaderSection.tsx
+++ src/sections/HeaderSection.tsx
@@ -1,7 +1,32 @@
+import Image from 'next/image';
 import React from 'react';
+import { LogoSVG, ProfileSVG, SearchSVG, ShopSVG } from '../../public';
+import { IoIosMenu } from 'react-icons/io';
 
 function HeaderSection() {
-  return <header></header>;
+  return (
+    <header>
+      <div className='flex justify-between px-[1%]  bg-[#000000] py-6 items-center'>
+        <div className='w-[42px] h-[42px] text-2xl text-white   flex items-center justify-center cursor-pointer hover:bg-[#333333] duration-450'>
+          <IoIosMenu />
+        </div>
+        <Image
+          src={LogoSVG}
+          alt='logo'
+          className='object-contain w-[200px] h-[50px] cursor-pointer'
+        />
+        <div className='flex gap-6 '>
+          <Image src={SearchSVG} alt='search icon' className='cursor-pointer' />
+          <Image
+            src={ProfileSVG}
+            alt='go to Profile icon'
+            className='cursor-pointer'
+          />
+          <Image src={ShopSVG} alt='cart icon' className='cursor-pointer' />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default HeaderSection;
